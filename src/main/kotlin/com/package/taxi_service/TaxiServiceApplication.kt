6c30776dbbf9a5ff6acf96fd2f71a3c4ced1d93f
barbara <rodriguessbarbara@hotmail.com>
2025-03-11 20:20:48 -0300

    :tada: primeiro commit

---
+++ src/main/kotlin/com/package/taxi_service/TaxiServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.`package`.taxi_service
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TaxiServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<TaxiServiceApplication>(*args)
+}
---
+++ src/test/kotlin/com/package/taxi_service/TaxiServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.`package`.taxi_service
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TaxiServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
